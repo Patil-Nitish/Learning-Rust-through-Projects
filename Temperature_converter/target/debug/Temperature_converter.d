873720ab689209cd802eb47e24c193b335df0f64
Nitish Patil <nitishp1929@gmail.com>
2025-07-05 19:07:29 +0530

    repair

--- Temperature_converter/target/debug/Temperature_converter.d
+++ Temperature_converter/target/debug/Temperature_converter.d
@@ -1,1 +1,1 @@
-D:\Rust\Temperature_converter\target\debug\Temperature_converter.exe: D:\Rust\Temperature_converter\src\main.rs
+D:\Rust\ Projects\Learning-Rust-through-Projects\Temperature_converter\target\debug\Temperature_converter.exe: D:\Rust\ Projects\Learning-Rust-through-Projects\Temperature_converter\src\main.rs
